5e5ac23ff2826e95e010db745709c55309a3f0a4
Elliot Larson <elliot@onehouse.net>
2017-09-10 21:39:35 -0700

    Initial work on toggle command

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { blockToggle } from './block-toggle';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,16 +10,38 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "ruby-around-the-block" is now active!');
+    // console.log('Congratulations, your extension "ruby-around-the-block" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    let disposable = vscode.commands.registerCommand('extension.rubyBlockToggle', () => {
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        // 1. get full text of current document
+        const startLinePos = new vscode.Position(0, 0);
+        const endLinePos = new vscode.Position(editor.document.lineCount, 0);
+        const codeRange = new vscode.Range(startLinePos, endLinePos);
+        const editorLines = editor.document.getText(codeRange).split(/\n/);
+
+        // 2. get current line
+        const currentLineNumber: number = editor.selection.active.line;
+
+        // 3. call block toggle
+        const replacementCode = blockToggle(editorLines, currentLineNumber);
+
+        // 4. swap in block toggle replacement lines
+        // ...
+
+
+        // var selection = editor.selection;
+        // var text = editor.document.getText(selection);
+
+        // // Display a message box to the user
+        // vscode.window.showInformationMessage('Selected characters: ' + text.length);
     });
 
     context.subscriptions.push(disposable);
