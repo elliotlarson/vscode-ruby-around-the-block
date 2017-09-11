d862e1753487f6a495f5c69ef1ae01aa3064229c
Elliot Larson <elliot@onehouse.net>
2017-09-10 21:38:51 -0700

    Adding in blockToggle functionality

---
+++ src/block-toggle.ts
@@ -0,0 +1,177 @@
+export interface ToggledCode {
+  replacementLines: string[];
+  startLineNum: number;
+  endLineNum: number;
+}
+
+type BlockMarker = 'doend' | 'brace';
+
+interface BlockMarkerPosition {
+  lineNum: number;
+  columnNum: number;
+  blockType: BlockMarker;
+}
+
+interface BlockMarkerSearch {
+  markerRegex: RegExp;
+  blockType: BlockMarker;
+}
+
+const START_DOEND_BLOCK_REGEXP = /do\b/;
+
+const START_SEARCHES: BlockMarkerSearch[] = [
+  { markerRegex: START_DOEND_BLOCK_REGEXP, blockType: 'doend' },
+  { markerRegex: /{/, blockType: 'brace' },
+];
+
+const END_DOEND_BLOCK_REGEXP = /end\b/;
+
+const END_SEARCHES: BlockMarkerSearch[] = [
+  { markerRegex: END_DOEND_BLOCK_REGEXP, blockType: 'doend' },
+  { markerRegex: /}$/, blockType: 'brace' },
+];
+
+const COMMENT_REGEXP = /#[^{].*$/;
+
+export function findBlockStart(
+  lines: string[], currentLineNum: number
+): BlockMarkerPosition {
+  return findBlock(lines, currentLineNum, -1, START_SEARCHES);
+}
+
+function stripComment(line: string): string {
+  return line.replace(COMMENT_REGEXP, '');
+}
+
+export function findBlockEnd(
+  lines: string[],
+  currentLineNum: number,
+  blockType: null | BlockMarker = null,
+): BlockMarkerPosition {
+  return findBlock(lines, currentLineNum, +1, END_SEARCHES, blockType);
+}
+
+function findBlock(
+  lines: string[],
+  currentLineNum: number,
+  incrementor: number,
+  blockMarkerSearches: BlockMarkerSearch[],
+  blockType: null | BlockMarker = null,
+): BlockMarkerPosition {
+  const currentLine = rtrim(stripComment(lines[currentLineNum]));
+  for (let search of blockMarkerSearches) {
+    const onlyFindingForSpecficType: boolean =
+      blockType !== null && search.blockType !== blockType;
+    if (onlyFindingForSpecficType) { continue; }
+    let matches = search.markerRegex.exec(currentLine);
+    if (matches === null) { continue; }
+    return {
+      lineNum: currentLineNum,
+      columnNum: matches.index,
+      blockType: search.blockType,
+    };
+  }
+  const newLineNum = currentLineNum + incrementor;
+  return findBlock(
+    lines, newLineNum, incrementor, blockMarkerSearches, blockType
+  );
+}
+
+function rtrim(str: string): string {
+  return str.replace(/~+$/, '');
+}
+
+export interface CurrentBlockDescriptor {
+  blockType: BlockMarker;
+  startLineNum: number;
+  startColumnNum: number;
+  endLineNum: number;
+  endColumnNum: number;
+}
+
+export function getCurrentBlockDescriptor(
+  lines: string[], currentLineNum: number
+): CurrentBlockDescriptor {
+  const blockStart = findBlockStart(lines, currentLineNum);
+  const blockEnd = findBlockEnd(lines, currentLineNum, blockStart.blockType);
+  return {
+    blockType: blockStart.blockType,
+    startLineNum: blockStart.lineNum,
+    startColumnNum: blockStart.columnNum,
+    endLineNum: blockEnd.lineNum,
+    endColumnNum: blockEnd.columnNum,
+  };
+}
+
+export function getDoendReplacementBlockLines(
+  lines: string[], currentBlockDescriptor: CurrentBlockDescriptor,
+): string[] {
+  let blockLines = lines.slice(
+    currentBlockDescriptor.startLineNum,
+    currentBlockDescriptor.endLineNum + 1,
+  );
+
+  const doLength = 2;
+  const endLength = 3;
+
+  let startIndex = currentBlockDescriptor.startColumnNum;
+  let endIndex = startIndex + doLength;
+  let lineIndex = 0;
+  let line = blockLines[lineIndex].replace(START_DOEND_BLOCK_REGEXP, '{');
+  blockLines[lineIndex] = line;
+
+  startIndex = currentBlockDescriptor.endColumnNum;
+  endIndex = startIndex + endLength;
+  lineIndex = blockLines.length - 1;
+  line = blockLines[lineIndex].replace(END_DOEND_BLOCK_REGEXP, '}');
+  blockLines[lineIndex] = line;
+
+  if (blockLines.length > 3) { return blockLines };
+
+  const indentSpaces = /^\s*/.exec(blockLines[0]);
+  const blockLinesStr = blockLines.join(' ')
+    .replace(/\s+/g, ' ') // remove extra spaces
+    .replace(/^\s*/, indentSpaces[0]); // add back indent spaces
+  return [blockLinesStr];
+}
+
+export function getBraceReplacementBlockLines(
+  lines: string[], currentBlockDescriptor: CurrentBlockDescriptor,
+): string[] {
+  let blockLines = lines.slice(
+    currentBlockDescriptor.startLineNum,
+    currentBlockDescriptor.endLineNum + 1,
+  );
+  const startCol = currentBlockDescriptor.startColumnNum;
+  const endCol = currentBlockDescriptor.endColumnNum;
+
+  if (blockLines.length === 1) {
+    const braceLine = blockLines[0];
+    const indent = braceLine.match(/^\s*/)[0];
+    return [
+      `${braceLine.substring(0, startCol)}do`,
+      `${indent}  ${braceLine.substring(startCol + 1, endCol).trim()}`,
+      `${indent}end`,
+    ];
+  } else {
+    blockLines[0] = `${blockLines[0].substring(0, startCol)}do`;
+    const lastInd = blockLines.length - 1;
+    blockLines[lastInd] = `${blockLines[lastInd].substring(0, endCol)}end`;
+    return blockLines;
+  }
+}
+
+export function blockToggle(lines: string[], currentLineNum: number): ToggledCode {
+  const currentBlockDescriptor = getCurrentBlockDescriptor(lines, currentLineNum);
+  let replacementLines
+  if (currentBlockDescriptor.blockType == 'doend') {
+    replacementLines = getDoendReplacementBlockLines(lines, currentBlockDescriptor);
+  } else {
+    replacementLines = getBraceReplacementBlockLines(lines, currentBlockDescriptor);
+  }
+  return {
+    replacementLines: replacementLines,
+    startLineNum: currentBlockDescriptor.startLineNum,
+    endLineNum: currentBlockDescriptor.endLineNum,
+  };
+}
---
+++ test/block-toggle.test.ts
@@ -0,0 +1,464 @@
+import {
+  getCurrentBlockDescriptor,
+  findBlockStart,
+  findBlockEnd,
+  CurrentBlockDescriptor,
+  getDoendReplacementBlockLines,
+  getBraceReplacementBlockLines,
+  blockToggle,
+} from '../src/block-toggle';
+import { expect } from 'chai';
+import 'mocha';
+
+let doEndBlock = `
+  describe 'my test' do
+    let(:foo) do
+      create(:foo)
+    end
+  end
+  `;
+
+let doEndBlockSingleLine = `
+  describe 'my test' do
+    let(:foo) do create(:foo) end
+  end
+  `;
+
+let doEndBlockWithComment = `
+  describe 'my test' do
+    let(:foo) do
+      # a comment with do and end in it
+      create(:foo)
+      # here's another comment
+    end
+  end
+  `;
+
+let bracesOneLineBlock = `
+  describe 'my test' do
+    let(:foo) { create(:foo) }
+  end
+  `;
+
+let doEndBlockWithArgument = `
+  def my_method(array_name_with_do_in_it)
+    array_name_with_do_in_it.map do |item|
+      item.upcase
+    end
+  end
+  `;
+
+let bracesBlockWithArgument = `
+  def my_method(array_name_with_do_in_it)
+    array_name_with_do_in_it.map { |item| item.upcase }
+  end
+  `;
+
+let bracesBlockWithHash = `
+  describe 'my test' do
+    let(:foo) { { foo: 'bar' } }
+  end
+  `;
+
+let doEndBlockWithHash = `
+  describe 'my test' do
+    let(:foo) do
+      { foo: 'bar' }
+    end
+  end
+  `;
+
+let bracesBlockWithComment = `
+  describe 'my test' do
+    let(:foo) {
+      # a comment with do and end in it
+      create(:foo)
+      # here's another comment
+    }
+  end
+  `;
+
+let bracesBlockWithMultipleLines = `
+  describe 'my test' do
+    let(:foo) {
+      create(:foo)
+    }
+  end
+  `;
+
+describe('findBlockStart()', () => {
+  const testCases = [
+    {
+      codeString: 'doEndBlock',
+      cursorLine: 3,
+      lineNum: 2,
+      columnNum: 14,
+      blockType: 'doend',
+    },
+    {
+      codeString: 'doEndBlockSingleLine',
+      cursorLine: 2,
+      lineNum: 2,
+      columnNum: 14,
+      blockType: 'doend',
+    },
+    {
+      codeString: 'doEndBlock',
+      cursorLine: 2,
+      lineNum: 2,
+      columnNum: 14,
+      blockType: 'doend',
+    },
+    {
+      codeString: 'bracesOneLineBlock',
+      cursorLine: 2,
+      lineNum: 2,
+      columnNum: 14,
+      blockType: 'brace',
+    },
+    {
+      codeString: 'doEndBlockWithComment',
+      cursorLine: 4,
+      lineNum: 2,
+      columnNum: 14,
+      blockType: 'doend',
+    },
+    {
+      codeString: 'doEndBlockWithArgument',
+      cursorLine: 3,
+      lineNum: 2,
+      columnNum: 33,
+      blockType: 'doend',
+    },
+    {
+      codeString: 'bracesBlockWithHash',
+      cursorLine: 2,
+      lineNum: 2,
+      columnNum: 14,
+      blockType: 'brace',
+    },
+  ];
+
+  for (let testCase of testCases) {
+    let codeString = testCase.codeString;
+    let cursorLine = testCase.cursorLine;
+
+    context(`${codeString} with cursor on line ${cursorLine}`, () => {
+      let blockStart;
+
+      beforeEach(() => {
+        const codeLines = eval(testCase.codeString).split('\n');
+        blockStart = findBlockStart(codeLines, testCase.cursorLine);
+      });
+
+      it('finds the correct block start line num', () => {
+        expect(blockStart.lineNum).to.eq(testCase.lineNum);
+      });
+
+      it('finds the correct block start column num', () => {
+        expect(blockStart.columnNum).to.eq(testCase.columnNum);
+      });
+
+      it('finds the correct block type', () => {
+        expect(blockStart.blockType).to.eq(testCase.blockType);
+      });
+    });
+  }
+});
+
+describe('findBlockEnd()', () => {
+  const testCases = [
+    {
+      codeString: 'doEndBlock',
+      cursorLine: 3,
+      lineNum: 4,
+      columnNum: 4,
+      blockType: 'doend',
+    },
+    {
+      codeString: 'doEndBlockSingleLine',
+      cursorLine: 2,
+      lineNum: 2,
+      columnNum: 30,
+      blockType: 'doend',
+    },
+    {
+      codeString: 'doEndBlock',
+      cursorLine: 2,
+      lineNum: 4,
+      columnNum: 4,
+      blockType: 'doend',
+    },
+    {
+      codeString: 'bracesOneLineBlock',
+      cursorLine: 2,
+      lineNum: 2,
+      columnNum: 29,
+      blockType: 'brace',
+    },
+    {
+      codeString: 'doEndBlockWithComment',
+      cursorLine: 4,
+      lineNum: 6,
+      columnNum: 4,
+      blockType: 'doend',
+    },
+    {
+      codeString: 'doEndBlockWithArgument',
+      cursorLine: 3,
+      lineNum: 4,
+      columnNum: 4,
+      blockType: 'doend',
+    },
+    {
+      codeString: 'bracesBlockWithHash',
+      cursorLine: 2,
+      lineNum: 2,
+      columnNum: 31,
+      blockType: 'brace',
+    },
+  ];
+
+  for (let testCase of testCases) {
+    let codeString = testCase.codeString;
+    let cursorLine = testCase.cursorLine;
+
+    context(`${codeString} with cursor on line ${cursorLine}`, () => {
+      let blockStart;
+
+      beforeEach(() => {
+        const codeLines = eval(testCase.codeString).split('\n');
+        blockStart = findBlockEnd(codeLines, testCase.cursorLine);
+      });
+
+      it('finds the correct block start line num', () => {
+        expect(blockStart.lineNum).to.eq(testCase.lineNum);
+      });
+
+      it('finds the correct block end column num', () => {
+        expect(blockStart.columnNum).to.eq(testCase.columnNum);
+      });
+
+      it('finds the correct block type', () => {
+        expect(blockStart.blockType).to.eq(testCase.blockType);
+      });
+    });
+  }
+
+  describe('defining a specific block end type', () => {
+    context('doend block with a hash in it', () => {
+      let blockEnd;
+      const testCase = {
+        codeString: 'doEndBlockWithHash',
+        cursorLine: 2,
+        lineNum: 4,
+        columnNum: 4,
+        blockType: 'doend',
+      };
+
+      beforeEach(() => {
+        const codeLines = eval(testCase.codeString).split('\n');
+        blockEnd = findBlockEnd(codeLines, testCase.cursorLine, 'doend');
+      });
+
+      it('finds the correct block end line num', () => {
+        expect(blockEnd.lineNum).to.eq(testCase.lineNum);
+      });
+
+      it('finds the correct block end column num', () => {
+        expect(blockEnd.columnNum).to.eq(testCase.columnNum);
+      });
+
+      it('finds the correct block type', () => {
+        expect(blockEnd.blockType).to.eq(testCase.blockType);
+      });
+    });
+  });
+
+  context('a set of code lines that do not contain a block', () => {});
+});
+
+describe('currentBlock()', () => {
+  context('with do end block', () => {
+    let block;
+    beforeEach(() => {
+      const currentLineNum = 2;
+      block = getCurrentBlockDescriptor(doEndBlock.split('\n'), currentLineNum);
+    });
+
+    it('returns the correct type of block', () => {
+      expect(block.blockType).to.eq('doend');
+    });
+
+    it('returns the expected starting line', () => {
+      expect(block.startLineNum).to.eq(2);
+    });
+
+    it('returns the expected starting column', () => {
+      expect(block.startColumnNum).to.eq(14);
+    });
+
+    it('returns the correct end line', () => {
+      expect(block.endLineNum).to.eq(4);
+    });
+
+    it('returns the correct end column', () => {
+      expect(block.endColumnNum).to.eq(4);
+    });
+  })
+});
+
+describe('getDoendReplacementBlockLines()', () => {
+  context('with do end block', () => {
+    it('returns the expected replacement lines', () => {
+      const currentBlockDescriptor: CurrentBlockDescriptor = {
+        blockType: 'doend',
+        startLineNum: 2,
+        startColumnNum: 14,
+        endLineNum: 4,
+        endColumnNum: 4,
+      };
+      const lines = doEndBlock.split('\n');
+      const expectedReplacementLines = [bracesOneLineBlock.split('\n')[2]];
+      const replacementLines =
+        getDoendReplacementBlockLines(lines, currentBlockDescriptor);
+      expect(replacementLines).to.deep.eq(expectedReplacementLines);
+    });
+  });
+
+  context('with single line doend block', () => {
+    it('returns the expected replacement lines', () => {
+      const currentBlockDescriptor: CurrentBlockDescriptor = {
+        blockType: 'doend',
+        startLineNum: 2,
+        startColumnNum: 14,
+        endLineNum: 2,
+        endColumnNum: 30,
+      };
+      const lines = doEndBlockSingleLine.split('\n');
+      const expectedReplacementLines = [bracesOneLineBlock.split('\n')[2]];
+      const replacementLines =
+        getDoendReplacementBlockLines(lines, currentBlockDescriptor);
+      expect(replacementLines).to.deep.eq(expectedReplacementLines);
+    });
+  });
+
+  context('doend block with arguments', () => {
+    it('returns the expected replacement lines', () => {
+      const currentBlockDescriptor: CurrentBlockDescriptor = {
+        blockType: 'doend',
+        startLineNum: 2,
+        startColumnNum: 33,
+        endLineNum: 4,
+        endColumnNum: 4,
+      };
+      const lines = doEndBlockWithArgument.split('\n');
+      const expectedReplacementLines = [bracesBlockWithArgument.split('\n')[2]];
+      const replacementLines =
+        getDoendReplacementBlockLines(lines, currentBlockDescriptor);
+      expect(replacementLines).to.deep.eq(expectedReplacementLines);
+    });
+  });
+
+  context('doend block with multiple lines', () => {
+    it('returns the expected replacement lines', () => {
+      const currentBlockDescriptor: CurrentBlockDescriptor = {
+        blockType: 'doend',
+        startLineNum: 2,
+        startColumnNum: 14,
+        endLineNum: 6,
+        endColumnNum: 4,
+      };
+      const lines = doEndBlockWithComment.split('\n');
+      const expectedReplacementLines =
+        bracesBlockWithComment.split('\n').slice(2, 7);
+      const replacementLines =
+        getDoendReplacementBlockLines(lines, currentBlockDescriptor);
+      expect(replacementLines).to.deep.eq(expectedReplacementLines);
+    });
+  });
+});
+
+describe('getBraceReplacementBlockLines()', () => {
+  context('with single line brace block', () => {
+    it('returns the expected replacement lines', () => {
+      const currentBlockDescriptor: CurrentBlockDescriptor = {
+        blockType: 'brace',
+        startLineNum: 2,
+        startColumnNum: 14,
+        endLineNum: 2,
+        endColumnNum: 29,
+      };
+      const lines = bracesOneLineBlock.split('\n');
+      const expectedReplacementLines = doEndBlock.split('\n').slice(2, 5);
+      const replacementLines =
+        getBraceReplacementBlockLines(lines, currentBlockDescriptor);
+      expect(replacementLines).to.deep.eq(expectedReplacementLines);
+    });
+  });
+
+  context('with multi line brace block', () => {
+    it('returns the expected replacement lines', () => {
+      const currentBlockDescriptor: CurrentBlockDescriptor = {
+        blockType: 'brace',
+        startLineNum: 2,
+        startColumnNum: 14,
+        endLineNum: 4,
+        endColumnNum: 4,
+      };
+      const lines = bracesBlockWithMultipleLines.split('\n');
+      const expectedReplacementLines = doEndBlock.split('\n').slice(2, 5);
+      const replacementLines =
+        getBraceReplacementBlockLines(lines, currentBlockDescriptor);
+      expect(replacementLines).to.deep.eq(expectedReplacementLines);
+    });
+  });
+});
+
+describe('blockToggle()', () => {
+  let toggled;
+
+  context('with a doEnd block', () => {
+    const lines = doEndBlock.split('\n');
+    const replacementLines = [bracesOneLineBlock.split('\n')[2]];
+
+    beforeEach(() => {
+      const currentLineNum = 2;
+      toggled = blockToggle(lines, currentLineNum);
+    });
+
+    it('returns the expected lines', () => {
+      expect(toggled.replacementLines).to.deep.eq(replacementLines);
+    });
+
+    it('returns the expected lineStart', () => {
+      expect(toggled.startLineNum).to.eq(2);
+    });
+
+    it('returns the expected lineEnd', () => {
+      expect(toggled.endLineNum).to.eq(4);
+    });
+  });
+
+  context('with a brace block', () => {
+    const lines = bracesOneLineBlock.split('\n');
+    const replacementLines = doEndBlock.split('\n').slice(2, 5);
+
+    beforeEach(() => {
+      const currentLineNum = 2;
+      toggled = blockToggle(lines, currentLineNum);
+    });
+
+    it('returns the expected lines', () => {
+      expect(toggled.replacementLines).to.deep.eq(replacementLines);
+    });
+
+    it('returns the expected lineStart', () => {
+      expect(toggled.startLineNum).to.eq(2);
+    });
+
+    it('returns the expected lineEnd', () => {
+      expect(toggled.endLineNum).to.eq(2);
+    });
+  });
+});
